3c073897239abf6e59765b9e77a678a9599a6616
Hassaan Saleem <saleemhassaan94@gmail.com>
2018-11-06 10:40:23 +0500

    Added 3 classes

---
+++ studentAdvisorySystem/src/TokyoUniversity.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentadvisorysystem;
+
+/**
+ *
+ * @author Hassaan
+ */
+public class TokyoUniversity {
+    private String name;
+    private String location;
+    private String HECregistrationNumber;
+
+    public void setHECregistrationNumber(String HECregistrationNumber) {
+        this.HECregistrationNumber = HECregistrationNumber;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
---
+++ studentAdvisorySystem/src/person.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentadvisorysystem;
+
+/**
+ *
+ * @author Hassaan
+ */
+public class person {
+    
+    private String name;
+    private char gender;
+    private String Address;
+    private String age;
+    private String NIC;
+    private String CoNo;
+
+    public void setAddress(String Address) {
+        this.Address = Address;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public void setCoNo(String CoNo) {
+        this.CoNo = CoNo;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public void setNIC(String NIC) {
+        this.NIC = NIC;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+       
+}
