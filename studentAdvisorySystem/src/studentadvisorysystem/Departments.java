c7deb594501e8901bd28b0e772ffa8f87509cb37
Hassaan Saleem <saleemhassaan94@gmail.com>
2018-11-06 10:50:43 +0500

    dept class added

---
+++ studentAdvisorySystem/src/studentadvisorysystem/Departments.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentadvisorysystem;
+
+/**
+ *
+ * @author Hassaan
+ */
+public class Departments {
+    
+    private String name;
+    private String id;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
